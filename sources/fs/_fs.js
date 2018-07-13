955306c1529aca92a38f5e7b46632295d7b84f82
Tristan VALCKE <valcketristan@gmail.com>
2018-07-13 16:03:09 +0200

    Add utils about fs

---
+++ sources/fs/_fs.js
@@ -0,0 +1,18 @@
+/**
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [MIT]{@link https://opensource.org/licenses/MIT}
+ *
+ * @module sources/cores/cores
+ * @description This is the cores main export entry point.
+ * It expose all exports of the voids, booleans, numbers, symbols, strings, arrays, objects and functions validators.
+ *
+ */
+
+export * from './arrays'
+export * from './booleans'
+export * from './functions'
+export * from './numbers'
+export * from './objects'
+export * from './strings'
+export * from './symbols'
+export * from './voids'
