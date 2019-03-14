3d6355b3e5df6c4707122b705e325a70ecae28ae
Tristan VALCKE <valcketristan@gmail.com>
2019-03-14 17:13:55 +0100

    Geomathics: Remove unexported and already implemented method randomInt

--- sources/geomathics/_geomathics.js
+++ sources/geomathics/_geomathics.js
@@ -8,16 +8,3 @@
  */
 
 export * from './trigonometries'
-
-/**
- * Returns a random integer between min (inclusive) and max (inclusive)
- * Using Math.round() will give you a non-uniform distribution!
- *
- * @param min - The minimum (inclusive) range value for random number
- * @param max - The maximum (inclusive) range value for random number
- * @returns {Number} - A random number between man and max
- * @private
- */
-function randomInt ( min, max ) {
-    return Math.floor( Math.random() * ( max - min + 1 ) ) + min
-}
