500fdc05f27aefe78f8f4ce6e7a3f457b20cf9bb
Tristan VALCKE <valcketristan@gmail.com>
2019-07-31 23:47:58 +0200

    refactor(file-system): rename fs to file-system
    Rename the fs folder and file to file-system to apply new itee standard
    
    BREAKING CHANGE: The fs folder is not file-system, it break any package relying on the intermediary
    exporter file fs.js.

--- sources/itee-utils.js
+++ sources/itee-utils.js
@@ -7,7 +7,7 @@
  */
 
 export * from './cores/_cores'
-export * from './fs/_fs'
+export * from './file-system/_file-system'
 export * from './geomathics/_geomathics'
 export * from './physics/_physics'
 export * from './testings/_testings'
