444a460b5d28d34b8758ac36486f634aa2dce804
Tristan VALCKE <valcketristan@gmail.com>
2019-07-31 23:49:01 +0200

    docs(global): update licence to BSD-3

--- tests/benchmarks/itee-utils.benchs.js
+++ tests/benchmarks/itee-utils.benchs.js
@@ -1,6 +1,6 @@
 /**
  * @author [Tristan Valcke]{@link https://github.com/Itee}
- * @license [MIT]{@link https://opensource.org/licenses/MIT}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
  *
  */
 
