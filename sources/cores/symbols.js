22c7da7962eb7696ad14b7f219132b2f856fe9b0
Tristan VALCKE <valcketristan@gmail.com>
2019-07-31 23:48:36 +0200

    docs(global): update licence to BSD-3

--- sources/cores/symbols.js
+++ sources/cores/symbols.js
@@ -1,6 +1,6 @@
 /**
  * @author [Tristan Valcke]{@link https://github.com/Itee}
- * @license [MIT]{@link https://opensource.org/licenses/MIT}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
  *
  * @module sources/cores/symbols
  * @description Export the utilities methods about symbols
