fc975c8338ba25ad44465cf7a9c514bf853e5fb7
Tristan VALCKE <valcketristan@gmail.com>
2019-08-04 15:23:16 +0200

    fix(rollupconfig): add itee-validators as dependencies for all bundle
    This allow dependents package to correctly resolve the itee-validators package and perform better
    tree shaking.

--- configs/rollup.conf.js
+++ configs/rollup.conf.js
@@ -85,12 +85,9 @@ function CreateRollupConfigs ( options ) {
             const outputPath = ( isProd ) ? path.join( output, `${fileName}.${format}.min.js` ) : path.join( output, `${fileName}.${format}.js` )
 
             configs.push( {
-                input:    input,
-                external: ( format === 'cjs' ) ? [
-                    'fs',
-                    'path'
-                ] : [],
-                plugins: [
+                input:     input,
+                external:  ( format === 'cjs' ) ? [ 'fs', 'path', 'itee-validators' ] : [ 'itee-validators' ],
+                plugins:   [
                     replace( {
                         defines: {
                             IS_REMOVE_ON_BUILD:  false,
