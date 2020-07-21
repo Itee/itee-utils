7e3ecceef824232892a4a0a3f1604ea33222dac6
Tristan VALCKE <valcketristan@gmail.com>
2020-07-21 10:14:29 +0200

    feat(number): normalize random function and add inclusive version for float and integer

--- sources/cores/numbers.js
+++ sources/cores/numbers.js
@@ -14,15 +14,36 @@ export function getRandom () {
 /**
  * Returns a random number between min (inclusive) and max (exclusive)
  */
-export function getRandomArbitrary ( min = 0, max = 1 ) {
+export function getRandomFloatExclusive ( min = 0.0, max = 1.0 ) {
     return Math.random() * ( max - min ) + min
+}
+
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+export function getRandomFloatInclusive ( min = 0.0, max = 1.0 ) {
+    return Math.random() * ( max - min + 1.0 ) + min
 }
 
 /**
  * Returns a random integer between min (inclusive) and max (inclusive)
  * Using Math.round() will give you a non-uniform distribution!
  */
-export function getRandomInt ( min = 0, max = 1 ) {
-    return ( Math.floor( Math.random() * ( max - min + 1 ) ) + min )
+export function getRandomIntExclusive ( min = 0, max = 1 ) {
+    const _min = Math.ceil( min )
+    const _max = Math.floor( max )
+    return ( Math.floor( Math.random() * ( _max - _min ) ) + _min )
+}
+
+// On renvoie un entier aléatoire entre une valeur min (incluse)
+// et une valeur max (incluse).
+// Attention : si on utilisait Math.round(), on aurait une distribution
+// non uniforme !
+export function getRandomIntInclusive ( min = 0, max = 1 ) {
+    const _min = Math.ceil( min )
+    const _max = Math.floor( max )
+    return Math.floor( Math.random() * ( _max - _min + 1 ) ) + _min
+}
+
 }
 
