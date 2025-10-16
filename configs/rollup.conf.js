0c0eb59e795fe34b669b33532c1b0686ede2b911
Tristan VALCKE <valcketristan@gmail.com>
2025-10-16 22:22:49 +0200

    refactor(rollup.conf): update banner generation and use figlet instead

--- configs/rollup.conf.js
+++ configs/rollup.conf.js
@@ -19,11 +19,31 @@ const commonjs        = require( '@rollup/plugin-commonjs' )
 const { nodeResolve } = require( '@rollup/plugin-node-resolve' )
 const terser          = require( 'rollup-plugin-terser' ).terser
 const replace         = require( 'rollup-plugin-re' )
+const figlet          = require( 'figlet' )
 
-function _computeBanner ( name, format ) {
+function getPrettyPackageName() {
 
-    const packageName = name || packageInfos.name
-    let prettyFormat  = ''
+    let packageName = ''
+
+    const nameSplits = packageInfos.name.split( '-' )
+    for ( const nameSplit of nameSplits ) {
+        packageName += nameSplit.charAt( 0 ).toUpperCase() + nameSplit.slice( 1 ) + '.'
+    }
+    packageName = packageName.slice( 0, -1 )
+
+    return packageName
+
+}
+
+function getPrettyPackageVersion() {
+
+    return 'v' + packageInfos.version
+
+}
+
+function getPrettyFormatForBanner( format ) {
+
+    let prettyFormat = ''
 
     switch ( format ) {
 
@@ -48,7 +68,43 @@ function _computeBanner ( name, format ) {
 
     }
 
-    return `console.log('${ packageName } v${ packageInfos.version } - ${ prettyFormat }')`
+    return prettyFormat
+
+}
+
+function _commentarize( banner ) {
+
+    let bannerCommented = '/**\n'
+    bannerCommented += ' * '
+    bannerCommented += banner.replaceAll( '\n', '\n * ' )
+    bannerCommented += '\n'
+    bannerCommented += ` * @desc    ${ packageInfos.description }\n`
+    bannerCommented += ' * @author  [Tristan Valcke]{@link https://github.com/Itee}\n'
+    bannerCommented += ' * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}\n'
+    bannerCommented += ' * \n'
+    bannerCommented += ' */'
+
+    return bannerCommented
+
+}
+
+function _computeBanner( format ) {
+
+    const packageName    = getPrettyPackageName()
+    const packageVersion = getPrettyPackageVersion()
+    const prettyFormat   = getPrettyFormatForBanner( format )
+
+    const figText = figlet.textSync(
+        `${ packageName } ${ packageVersion } - ${ prettyFormat }`,
+        {
+            font:             'Tmplr',
+            horizontalLayout: 'default',
+            verticalLayout:   'default',
+            whitespaceBreak:  true,
+        }
+    )
+
+    return _commentarize( figText )
 
 }
 
@@ -59,7 +115,7 @@ function _computeBanner ( name, format ) {
  * @param options
  * @return {Array.<json>} An array of rollup configuration
  */
-function CreateRollupConfigs ( options ) {
+function CreateRollupConfigs( options ) {
     'use strict'
 
     const {
@@ -131,7 +187,7 @@ function CreateRollupConfigs ( options ) {
 
                     // advanced options
                     paths:     {},
-                    banner:    ( isProd ) ? '' : _computeBanner( name, format ),
+                    banner:    ( isProd ) ? '' : _computeBanner( format ),
                     footer:    '',
                     intro:     ( format === 'iife' && !isProd ) ? 'if( iteeValidators === undefined ) { throw new Error(\'Itee.Utils need Itee.Validators to be defined first. Please check your scripts loading order.\') }' : '',
                     outro:     '',
