10e20d26bf9facc5b7766565fadce6d96f55b05a
Tristan VALCKE <valcketristan@gmail.com>
2025-11-27 11:10:38 +0100

    :bug: (benchmarks) use full filename on import to allow direct nodejs usage

--- sources/testings/benchmarks.js
+++ sources/testings/benchmarks.js
@@ -2,7 +2,7 @@
  * @author [Tristan Valcke]{@link https://github.com/Itee}
  * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
  */
-import * as globalDataMap from './primitives'
+import * as globalDataMap from './primitives.js'
 
 const Testing = {
 
