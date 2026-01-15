c56470fa6f530085c0239102d27479ada03dac47
Tristan VALCKE <valcketristan@gmail.com>
2026-01-15 21:02:06 +0100

    :green_heart: (patch) try to fix path again

--- .tasks/.patches/patch.task.mjs
+++ .tasks/.patches/patch.task.mjs
@@ -1,10 +1,14 @@
 import colors                   from 'ansi-colors'
 import log                      from 'fancy-log'
-import { relative }             from 'node:path'
 import {
     readFileSync,
     writeFileSync
 }                               from 'node:fs'
+import {
+    dirname,
+    relative
+}                               from 'node:path'
+import { fileURLToPath }        from 'node:url'
 import { join }                 from 'path'
 import { packageRootDirectory } from '../_utils.mjs'
 
@@ -13,6 +17,25 @@ const {
           blue
       } = colors
 
+function _getPackageRootDirectory() {
+
+    let __dirname
+
+    if ( import.meta.dirname ) {
+        __dirname = import.meta.dirname
+    } else if ( import.meta.filename ) {
+        __dirname = dirname( import.meta.filename )
+    } else if ( import.meta.url ) {
+        const __filename = fileURLToPath( import.meta.url )
+        __dirname        = dirname( __filename )
+    } else {
+        throw new Error( 'Unable to retrieve module dirname.' )
+    }
+
+    return join( __dirname, '../../' )
+
+}
+
 /**
  * @method npm run patch
  * @global
@@ -20,10 +43,15 @@ const {
  */
 const patchTask       = ( done ) => {
 
-    const filePath       = join( '../../', 'node_modules/itee-utils/sources/testings/benchmarks.js' )
+    const rootDir        = _getPackageRootDirectory()
+    const filePath       = join( rootDir, 'node_modules/itee-utils/sources/testings/benchmarks.js' )
+
+    log(rootDir)
+    log(filePath)
+    
     const fileBuffer     = readFileSync( filePath )
     const fileContent    = fileBuffer.toString()
-    const updatedContent = fileContent.replace( "import * as globalDataMap from './primitives'", "import * as globalDataMap from './primitives.js'" )
+    const updatedContent = fileContent.replace( 'import * as globalDataMap from \'./primitives\'', 'import * as globalDataMap from \'./primitives.js\'' )
     writeFileSync( filePath, updatedContent )
 
     done()
