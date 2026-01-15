e1e1e29c5e4ff2def8401e7c3bd91a521196c49c
Tristan VALCKE <valcketristan@gmail.com>
2026-01-15 21:33:18 +0100

    :arrow_up: (utils) upgrade internal reference to last version and remove previous patch stuff

--- .tasks/.patches/patch.task.mjs
+++ .tasks/.patches/patch.task.mjs
@@ -17,43 +17,12 @@ const {
           blue
       } = colors
 
-function _getPackageRootDirectory() {
-
-    let __dirname
-
-    if ( import.meta.dirname ) {
-        __dirname = import.meta.dirname
-    } else if ( import.meta.filename ) {
-        __dirname = dirname( import.meta.filename )
-    } else if ( import.meta.url ) {
-        const __filename = fileURLToPath( import.meta.url )
-        __dirname        = dirname( __filename )
-    } else {
-        throw new Error( 'Unable to retrieve module dirname.' )
-    }
-
-    return join( __dirname, '../../' )
-
-}
-
 /**
  * @method npm run patch
  * @global
  * @description Will apply some patch/replacements in dependencies
  */
 const patchTask       = ( done ) => {
-
-    const rootDir        = _getPackageRootDirectory()
-    const filePath       = join( rootDir, 'node_modules/itee-utils/sources/testings/benchmarks.js' )
-
-    log(rootDir)
-    log(filePath)
-    
-    const fileBuffer     = readFileSync( filePath )
-    const fileContent    = fileBuffer.toString()
-    const updatedContent = fileContent.replace( 'import * as globalDataMap from \'./primitives\'', 'import * as globalDataMap from \'./primitives.js\'' )
-    writeFileSync( filePath, updatedContent )
-
     done()
 }
 patchTask.displayName = 'patch'
