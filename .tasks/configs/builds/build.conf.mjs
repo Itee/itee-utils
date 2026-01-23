68fa2c837405b325c4562c2014aa66d287fbee74
Tristan VALCKE <valcketristan@gmail.com>
2026-01-23 10:53:43 +0100

    :art: (build.conf) identation

--- .tasks/configs/builds/build.conf.mjs
+++ .tasks/configs/builds/build.conf.mjs
@@ -2,8 +2,18 @@ import { createRollupConfigs } from '@itee/tasks/sources/utils/builds.mjs'
 
 export default createRollupConfigs( {
     externalMap: {
-        'esm':  [ 'node:fs', 'node:path', 'itee-validators' ],
-        'cjs':  [ 'node:fs', 'node:path', 'itee-validators' ],
-        'iife': [ 'itee-validators' ],
+        'esm':  [
+            'node:fs',
+            'node:path',
+            'itee-validators'
+        ],
+        'cjs':  [
+            'node:fs',
+            'node:path',
+            'itee-validators'
+        ],
+        'iife': [
+            'itee-validators'
+        ],
     }
 } )
